cb36d8a9faa9da960b7103e1273fdf7ca1903862
Victor-Kenji-Hirosue <kenjihvictor@gmail>
2026-06-23 10:28:54 -0300

    mudança nas estrelas

--- src/app/tela-inicio-component/tela-inicio-component.ts
+++ src/app/tela-inicio-component/tela-inicio-component.ts
@@ -8,19 +8,19 @@ import { Component } from '@angular/core';
 })
 export class TelaInicioComponent {
   meusJogos = [
-    { nome: 'Ultrakill', ano: 2019, nota: '4.9', imagem: 'jogos/Ultrakill.jpeg' },
-    { nome: 'Enter The Gungeon', ano: 2016, nota: '4.8', imagem: 'jogos/Enter the Gungeon.jpeg' },
-    { nome: 'Iron Lung', ano: 2022, nota: '4.7', imagem: 'jogos/Iron Lung.jpeg', },
-    { nome: 'DeltaRune', ano: 2018, nota: '4.9', imagem: 'jogos/Deltarune.jpeg',},
-    { nome: 'Mullet Madjack', ano: 2024, nota: '4.9', imagem: 'jogos/Mullet Madjack.jpeg',},
-    { nome: 'Dust: An Elysian Tail', ano: 2012, nota: '4.5', imagem: 'jogos/Dust.jpeg',},
-    { nome: 'Uktena 64', ano: 2025, nota: '4.5', imagem: 'jogos/Uktena 64.jpeg',},
-    { nome: 'Hotline Miami', ano: 2012, nota: '4.7', imagem: 'jogos/HM.jpeg' }
+    { nome: 'Ultrakill', ano: 2019, nota: 4.9, imagem: 'jogos/Ultrakill.jpeg' },
+    { nome: 'Enter The Gungeon', ano: 2016, nota: 4.8, imagem: 'jogos/Enter the Gungeon.jpeg' },
+    { nome: 'Iron Lung', ano: 2022, nota: 4.7, imagem: 'jogos/Iron Lung.jpeg', },
+    { nome: 'DeltaRune', ano: 2018, nota: 4.9, imagem: 'jogos/Deltarune.jpeg',},
+    { nome: 'Mullet Madjack', ano: 2024, nota: 4.9, imagem: 'jogos/Mullet Madjack.jpeg',},
+    { nome: 'Dust: An Elysian Tail', ano: 2012, nota: 4.5, imagem: 'jogos/Dust.jpeg',},
+    { nome: 'Uktena 64', ano: 2025, nota: 4.5, imagem: 'jogos/Uktena 64.jpeg',},
+    { nome: 'Hotline Miami', ano: 2012, nota: 4.7, imagem: 'jogos/HM.jpeg' }
   ];
 
   jogosPopulares = [
-    { nome: 'DeltaRune', ano: 2018, nota: '4.9', imagem: 'jogos/Deltarune.jpeg',},
-    { nome: 'Balatro', ano: 2024, nota: '4.2', imagem: 'jogos/balatro.jpeg' },
-    { nome: 'BALL X PIT', ano: 2025, nota: '4.5', imagem: 'jogos/Ball x pit.jpeg' }
+    { nome: 'DeltaRune', ano: 2018, nota: 4.9, imagem: 'jogos/Deltarune.jpeg',},
+    { nome: 'Balatro', ano: 2024, nota: 4.2, imagem: 'jogos/balatro.jpeg' },
+    { nome: 'BALL X PIT', ano: 2025, nota: 4.5, imagem: 'jogos/Ball x pit.jpeg' }
   ];
 }
