0092a0931527a16fc4e86fd5153f57d3fd5bb1e8
Vinicius Goncalo <goncalovinicius15@gmail.com>
2026-06-25 16:06:20 -0300

    Added ResenhaController.java

---
+++ BackEnd/game/src/main/java/com/stars/game/controllers/ResenhaController.java
@@ -0,0 +1,33 @@
+package com.stars.game.controllers;
+
+import com.stars.game.entities.Resenha;
+import com.stars.game.repositories.ResenhaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/resenhas")
+public class ResenhaController {
+
+    private final ResenhaRepository resenhaRepository;
+
+    public ResenhaController(ResenhaRepository resenhaRepository) {
+        this.resenhaRepository = resenhaRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Resenha>> listarTodas() {
+        return ResponseEntity.ok(resenhaRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Resenha> buscarPorId(@PathVariable Long id) {
+
+        Resenha resenha = resenhaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Resenha não encontrada"));
+
+        return ResponseEntity.ok(resenha);
+    }
+}
